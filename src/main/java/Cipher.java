809b185994b85011ce838ee97d54e3f6c3b5ee33
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-22 16:16:01 +0200

    create the encryption method to encrypt the inputed plain text

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,2 +1,20 @@
 public class Cipher {
+    public static final String myAlphabets = "abcdefghijklmnopqrstuvwxyz";
+    public static String encryption(String plainText, int myKey){
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+//        if (myKey > 26){
+//            myKey = (myKey % 26);
+//        }
+//        else if(myKey <0){
+//            myKey = (myKey % 26)+26;
+//        }
+        for (int j = 0; j< plainText.length(); j++){
+            int myPosition = myAlphabets.indexOf(plainText.charAt(j));
+            int myKeyValue = (myKey + myPosition) % 26;
+            char myValues = myAlphabets.charAt(myKeyValue);
+            cipherText += myValues;
+        }
+        return cipherText;
+    }
 }
--- src/main/java/Main_window.java
+++ src/main/java/Main_window.java
@@ -1,2 +1,6 @@
+import java.util.Scanner;
 public class Main_window {
+    public static void main(String[] args){
+
+    }
 }
