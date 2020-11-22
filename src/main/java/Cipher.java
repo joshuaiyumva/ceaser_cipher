8fe0bd144051e950d5c6168718a2226c50fdf7e1
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-22 16:51:56 +0200

    add a class called Main_Window that will holp the main function's execution part

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -3,12 +3,6 @@ public class Cipher {
     public static String encryption(String plainText, int myKey){
         plainText = plainText.toLowerCase();
         String cipherText = "";
-//        if (myKey > 26){
-//            myKey = (myKey % 26);
-//        }
-//        else if(myKey <0){
-//            myKey = (myKey % 26)+26;
-//        }
         for (int j = 0; j< plainText.length(); j++){
             int myPosition = myAlphabets.indexOf(plainText.charAt(j));
             int myKeyValue = (myKey + myPosition) % 26;
--- src/main/java/Main_window.java
+++ src/main/java/Main_window.java
@@ -1,6 +1,9 @@
+import java.io.Console;
 import java.util.Scanner;
 public class Main_window {
     public static void main(String[] args){
+        System.out.println("Welcome to my Ceaser Cipher App");
+
 
     }
 }
