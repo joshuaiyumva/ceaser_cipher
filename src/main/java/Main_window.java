caf0ba4d821d33fa087cc6fd0a256f922610bb05
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-22 21:51:37 +0200

    set new lines after the text

--- src/main/java/Main_window.java
+++ src/main/java/Main_window.java
@@ -3,7 +3,7 @@ public class Main_window {
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
-        System.out.println("Welcome to my Ceaser Cipher App");
+        System.out.println("Welcome to my Caeser Cipher App\n");
         for ( ;; )
         {
             System.out.println("Please make a choice...\n 1.Encryption \t 2.Decryption \t 3.Exit program\n" );
@@ -12,19 +12,19 @@ public class Main_window {
             Cipher ceaser_cipher = new Cipher();
             if (myChoice == 1)
             {
-                System.out.println("Enter your string to encrypt");
+                System.out.println("Enter your string to encrypt\n");
                 String message = new String();
                 message = s.next();
-                System.out.println("Enter Key (Key must be between 0 to 25)");
+                System.out.println("Enter Key (Key must be between 0 to 25)\n");
                 int key = s.nextInt();
                 System.out.println(ceaser_cipher.encryption(message, key));
             }
             else if (myChoice == 2)
             {
-                System.out.println("Enter your string to decrypt");
+                System.out.println("Enter your string to decrypt\n");
                 String message = new String();
                 message = s.next();
-                System.out.println("Enter Key used for the encryption (Key must be between 0 to 25)");
+                System.out.println("Enter Key used for the encryption (Key must be between 0 to 25)\n");
                 int key = s.nextInt();
                 System.out.println(ceaser_cipher.decryption(message, key));
             }
@@ -36,7 +36,6 @@ public class Main_window {
                 System.out.println("Sorry..., you made an invalid Choice");
             }
         }
-
 
     }
 }
